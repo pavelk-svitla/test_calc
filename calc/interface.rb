25d9ac9cb09c96b9522d83821e4b603b5d0a1d71
Pavel Klimenkov <ps.klimenkov@gmail.com>
2020-10-07 15:49:16 +0300

    user interface

---
+++ calc/interface.rb
@@ -0,0 +1,15 @@
+module Interface
+  HEADER = "===================================\n" \
+           "= Welcome to makeshift calculator =\n" \
+           "===================================\n" \
+           "=== please enter your commands ====\n" \
+           "===================================\n"
+
+  FOOTER = "===================================\n" \
+           "= Thank you for using calculator ==\n" \
+           "===================================\n"
+
+  MARKER = '=> '
+
+  NOT_IMPLEMENTED = 'ERROR: command is not recognized'
+end
